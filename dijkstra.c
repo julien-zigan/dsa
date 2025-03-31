8baa1fa74185a72f0113737f39e297ee57d94974
Julien Zigan <zigan.julien@gmail.com>
2025-03-31 15:55:15 +0200

    setup file structure for dijkstra: header, impl, and test

---
+++ dijkstra.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dijkstra.h"
+
+int main(void) 
+{
+    printf("\e[0;32mTest OK!\e[0;37m\n");
+    return EXIT_SUCCESS;
+}
---
+++ dijkstra.h
@@ -0,0 +1,35 @@
+#ifndef __DIJKSTRA_H__
+#define __DIJKSTRA_H__
+
+#define IDBUF 16
+
+struct node_t;
+
+typedef enum status_t {
+    PERMANENT,
+    TENTATIVE,
+    UNDEFINED
+} STATUS_T;
+
+typedef struct label_t {
+    int sum_weight;
+    struct node_t *next_hop;
+    STATUS_T status;
+} LABEL_T;
+
+
+typedef struct edge_t {
+    struct edge_t *next;
+    int weight;
+    struct node_t *node;
+
+} EDGE_T;
+
+typedef struct node_t {
+    struct node *next; 
+    char id[IDBUF];
+    EDGE_T *edges;
+    LABEL_T label;
+} NODE_T;
+
+#endif
